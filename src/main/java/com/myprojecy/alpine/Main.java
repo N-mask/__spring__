ee01ae761f84bb0256b4ee9add8b9cfe92a0b121
N-mask <nealzanwar@gmail.com>
2026-04-26 14:31:17 +0530

    init

--- src/main/java/com/myprojecy/alpine/Main.java
+++
@@ -1,7 +0,0 @@
-package com.myprojecy.alpine;
-
-public class Main {
-	public static void main(String[] args) {
-
-	}
-}
--- src/main/java/com/myprojecy/alpine/demoLayout.java
+++ src/main/java/com/myprojecy/alpine/demoLayout.java
@@ -1,19 +1,22 @@
 package com.myprojecy.alpine;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Background;
 
 @Route("/")
-public class demoLayout extends HorizontalLayout {
+public class demoLayout extends com.vaadin.flow.component.orderedlayout.VerticalLayout {
 	private static final long serialVersionUID = 2941347691437351255L;
 	private static int i = 0;
 
 	public demoLayout() {
+
 		Button button = new Button("Press me !");
 		button.addSingleClickListener(e -> {
 			System.out.println("Pressed button: " + (i++));
 		});
+		button.addThemeName(Background.SHADE_70);
 		add(button);
+		getStyle().set("background-color", "black");
 	}
 }
