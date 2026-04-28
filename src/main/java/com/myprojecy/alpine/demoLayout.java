ee07a9efde1750bce1cae7b73465817b7c4cfca1
N-mask <nealzanwar@gmail.com>
2026-04-28 09:46:41 +0530

    cleanup

--- src/main/java/com/myprojecy/alpine/demoLayout.java
+++ src/main/java/com/myprojecy/alpine/demoLayout.java
@@ -1,8 +1,6 @@
 package com.myprojecy.alpine;
 
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility.Background;
 
 @Route("/")
 public class demoLayout extends com.vaadin.flow.component.orderedlayout.VerticalLayout {
@@ -10,13 +8,5 @@ public class demoLayout extends com.vaadin.flow.component.orderedlayout.Vertical
 	private static int i = 0;
 
 	public demoLayout() {
-
-		Button button = new Button("Press me !");
-		button.addSingleClickListener(e -> {
-			System.out.println("Pressed button: " + (i++));
-		});
-		button.addThemeName(Background.SHADE_70);
-		add(button);
-		getStyle().set("background-color", "black");
 	}
 }
