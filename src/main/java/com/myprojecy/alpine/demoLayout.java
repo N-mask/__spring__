cf608bd2d5ac7bdc7e9f8b13ddbc6c939882ab7d
N-mask <nealzanwar@gmail.com>
2026-04-29 21:14:06 +0530

    cleanup

--- src/main/java/com/myprojecy/alpine/demoLayout.java
+++ src/main/java/com/myprojecy/alpine/demoLayout.java
@@ -1,12 +1,24 @@
 package com.myprojecy.alpine;
 
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.page.ColorScheme.Value;
 import com.vaadin.flow.router.Route;
 
 @Route("/")
-public class demoLayout extends com.vaadin.flow.component.orderedlayout.VerticalLayout {
+public class demoLayout extends AppLayout {
+
 	private static final long serialVersionUID = 2941347691437351255L;
-	private static int i = 0;
 
 	public demoLayout() {
+		this.getStyle().setBackgroundColor("black");
+		getStyle().setColor("WHITE");
+
+		Text t = new Text("Hello !");
+		HorizontalLayout header = new HorizontalLayout(t);
+
+		addToNavbar(header);
+		getStyle().setColorScheme(Value.DARK);
 	}
 }
